bd7c4921f1454eed237260209235e198ebb06261
Claudiarg13 <cromerog13@gmail.com>
2024-10-07 18:46:36 +0200

    "Has Ganado"
    Hecho el último paso

---
+++ ScriptGrupo6.bash
@@ -0,0 +1,16 @@
+import os
+from datetime import datetime
+
+def verificar_minutos_par_y_escribir(carpeta):
+    # Obtener la hora actual
+    minutos_actuales = datetime.now().minute
+
+    # Verificar si los minutos actuales son pares
+    if minutos_actuales % 2 == 0:
+        print("Has Ganado")
+        # Construir la ruta completa del archivo ganador.txt
+        ruta_archivo = os.path.join(carpeta, "ganador.txt")
+        
+        # Escribir "Has Ganado" en el archivo ganador.txt
+        with open(ruta_archivo, "w") as f:
+            f.write("Has Ganado")
